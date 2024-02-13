bde643d71277d66ac8aa793f27ee04364b7f3704
Anshuman Jha <anshumanjha0350@gmail.com>
2024-02-13 08:09:09 +0530

    canvas loded

--- src/canvas/Computers.jsx
+++ src/canvas/Computers.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
@@ -9,20 +9,20 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
-      <spotLight
+      <hemisphereLight intensity={1} groundColor='black' />
+       <spotLight
         position={[-20, 50, 10]}
-        angle={0.12}
+        angle={1}
         penumbra={1}
-        intensity={1}
+        intensity={50}
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
+      <pointLight intensity={10} /> 
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.95, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
